b6e65d21c5f7b3ac0169c10de883279a46296af1
santhiyamanickam <msanthiyatwilight@gmail.com>
2017-02-24 23:07:51 -0800

    Create stringtoint.java

---
+++ stringtoint.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class stringtoint
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String num="25";
+		int a=Integer.parseInt(num);
+		System.out.println(a);
+		
+	}
+}
