e09fb9679107512deb021701062cd658670b8894
santhiyamanickam <msanthiyatwilight@gmail.com>
2017-03-04 06:34:45 -0800

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class prime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,n,r=0,temp;
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		n=in.nextInt();
+		temp=n;
+		while(n>0)
+		{
+			a=n%10;
+			r=r*10+a;
+			n=n/10;
+			System.out.println(r);
+		}
+		if(temp==r)
+		{
+		System.out.println("This is prime number");
+		}
+		else
+		{
+			System.out.println("The number is not prime");
+		}
+		}
+	}
