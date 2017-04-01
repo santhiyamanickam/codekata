c245598cf1fc7665c4b2c050abf0bb8ded03ae4f
santhiyamanickam <msanthiyatwilight@gmail.com>
2017-03-31 19:16:31 -0700

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class helloworld
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for (int i=0;i<5;i++)
+			System.out.println("Hello world");
+
+	}
+}
