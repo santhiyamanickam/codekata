16d313dcab1699db409b051cdb6338c7132163bf
santhiyamanickam <msanthiyatwilight@gmail.com>
2017-03-31 19:11:15 -0700

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+class leapyear{
+public static void main(string args[])
+{
+Scanner in=new Scanner(Syatem.in);
+int in=b.nextInt;
+if(a%4==0)
+{
+System.out.println("This is a leap year";);
+}
+else 
+System.out.println("This is not a leap year");
+}
+}
